0cc14fdbbdb06a56d96d45516b5b2e3b86a9c769
senkochi <senkosannanoja@gmail.com>
2026-07-17 22:20:58 +0700

    Initial commit

---
+++ src/main/java/com/senko/astrail/AstrailApplication.java
@@ -0,0 +1,13 @@
+package com.senko.astrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstrailApplication.class, args);
+	}
+
+}
